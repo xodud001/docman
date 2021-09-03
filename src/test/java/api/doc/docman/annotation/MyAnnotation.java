a93d5ef3f2a665fe5ba983369bdfe492a4d1a477
김태영 <51165061+xodud001@users.noreply.github.com>
2021-09-03 23:23:55 +0900

    feat : 테스트용 커스텀 어노테이션 작성

---
+++ src/test/java/api/doc/docman/annotation/MyAnnotation.java
@@ -0,0 +1,13 @@
+package api.doc.docman.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface MyAnnotation {
+    String name();
+    String value() default "This is Default Value";
+}
---
+++ src/test/java/api/doc/docman/annotation/TheClass.java
@@ -0,0 +1,14 @@
+package api.doc.docman.annotation;
+
+public class TheClass {
+
+    @MyAnnotation(name = "Tae-yeong", value = "Hello World")
+    public void doThis(){
+
+    }
+
+    @MyAnnotation(name = "Tae-yeong2")
+    public void doThat(){
+
+    }
+}
