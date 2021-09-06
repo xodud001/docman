1947f252c34d4005165783cc4146e0e9caf485e0
xodud001 <51165061+xodud001@users.noreply.github.com>
2021-09-06 23:28:31 +0900

    test : Bean 만들어서 IoC 테스트

--- src/main/java/api/doc/docman/DocmanApplication.java
+++ src/main/java/api/doc/docman/DocmanApplication.java
@@ -4,6 +4,8 @@ import api.doc.docman.di.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class DocmanApplication {
@@ -15,15 +17,29 @@ public class DocmanApplication {
 		ApplicationContext context = ApplicationContextProvider.getContext();
 
 //		Base64Encoder base64Encoder = context.getBean(Base64Encoder.class);
-		Encoder encoder = context.getBean(Encoder.class);
+		Encoder encoder = context.getBean("base64Encode", Encoder.class);
 		String result = encoder.encode(url);
 		System.out.println(result);
- 
+
 //		UrlEncoder urlEncoder = context.getBean(UrlEncoder.class);
 //		encoder.setIEncoder(urlEncoder);
 //		result = encoder.encode(url);
 //		System.out.println(result);
 
 	}
+}
 
+@Configuration
+class AppConfig{
+
+	@Bean("base64Encode")
+	public Encoder encoder(Base64Encoder base64Encoder){
+		return new Encoder(base64Encoder);
+	}
+
+
+	@Bean("urlEncode")
+	public Encoder encoder(UrlEncoder urlEncoder){
+		return new Encoder(urlEncoder);
+	}
 }
