109212df94ef84ec99ad17b30638794ba3aa41ba
김태영 <51165061+xodud001@users.noreply.github.com>
2021-09-15 22:59:22 +0900

    test : Advice로 예외처리 테스트

---
+++ src/main/java/api/doc/docman/advice/UserControllerAdvice.java
@@ -0,0 +1,22 @@
+package api.doc.docman.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class UserControllerAdvice {
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity exception(Exception e){
+        System.out.println(e.getLocalizedMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("");
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity methodArgumentNotValidException(MethodArgumentNotValidException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+}
